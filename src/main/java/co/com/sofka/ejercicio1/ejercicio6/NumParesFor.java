6b96276d553beccadc8a3857d5423aa2e28709a4
Leonardo <leocv0205@gmail.com>
2021-09-05 18:07:54 -0500

    Se emplean mejoras al punto 7

--- src/main/java/co/com/sofka/ejercicio1/ejercicio6/NumParesFor.java
+++ src/main/java/co/com/sofka/ejercicio1/ejercicio6/NumParesFor.java
@@ -9,11 +9,17 @@ public final class NumParesFor extends NumPares {
 
     @Override
     public void imprimir() {
+        System.out.println("Lista de números pares");
         for (int i =1;i<=100;i++){
             if ((i % 2) == 0){
-                System.out.print(i + " ");
-            }else {
-                System.out.print(i + " ");
+                System.out.print(i + ", ");
+            }
+        }
+        System.out.println(" ");
+        System.out.println("Lista de números impares");
+        for (int j =1;j<=100;j++){
+            if ((j % 2) != 0){
+                System.out.print(j + ", ");
             }
         }
     }
